2f605173b3bacc54139284333d29fd1a670de966
Ahmad <khanyousafzaideveloper@gmail.com>
2023-03-22 12:42:34 +0500

    setonFocusChangeListner added , other discarded.

--- My_first_TV_APP/app/src/main/java/com/example/my_first_tv_app/MainActivity.java
+++ My_first_TV_APP/app/src/main/java/com/example/my_first_tv_app/MainActivity.java
@@ -1,40 +1,45 @@
 package com.example.my_first_tv_app;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-
 import androidx.fragment.app.FragmentActivity;
 
-/*
- * Main Activity class that loads {@link MainFragment}.
- */
-public class MainActivity extends FragmentActivity implements View.OnClickListener, View.OnKeyListener {
+public class MainActivity extends FragmentActivity implements View.OnClickListener, View.OnFocusChangeListener {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button b1 = findViewById(R.id.button2);
-        Button b2 = findViewById(R.id.button4);
-        Button b3 = findViewById(R.id.button5);
-        Button b4 = findViewById(R.id.button6);
-        b1.setOnClickListener(this);
-        b1.setOnKeyListener(this);
+        Button b2 = findViewById(R.id.button2);
+        Button b4 = findViewById(R.id.button4);
+        Button b5 = findViewById(R.id.button5);
+        Button b6 = findViewById(R.id.button6);
+        Button b3 = findViewById(R.id.button3);
+        Button b7 = findViewById(R.id.button7);
         b2.setOnClickListener(this);
-        b2.setOnKeyListener(this);
-        b3.setOnClickListener(this);
-        b3.setOnKeyListener(this);
+
         b4.setOnClickListener(this);
-        b4.setOnKeyListener(this);
+
+        b5.setOnClickListener(this);
+
+        b6.setOnClickListener(this);
+
+        b3.setOnClickListener(this);
+
+        b7.setOnClickListener(this);
+
+        b2.setOnFocusChangeListener(this);
+        b4.setOnFocusChangeListener(this);
+        b5.setOnFocusChangeListener(this);
+        b6.setOnFocusChangeListener(this);
+        b3.setOnFocusChangeListener(this);
+        b7.setOnFocusChangeListener(this);
+
     }
 
     @Override
@@ -58,18 +63,26 @@ public class MainActivity extends FragmentActivity implements View.OnClickListen
                 intent.setData(Uri.parse("https://www.youtube.com/watch?v=9Auq9mYxFEE"));
                 startActivity(intent);
                 break;
+            case R.id.button3:
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=dZvHWVKY-AQ"));
+                startActivity(intent);
+                break;
+            case R.id.button7:
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=O3DPVlynUM0"));
+                startActivity(intent);
+                break;
         }
     }
 
     @Override
-    public boolean onKey(View v, int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
-            v.setScaleX(1.5f);
-            v.setScaleY(1.5f);
-            return true; // consume the event
+    public void onFocusChange(View v, boolean b) {
+        if (b) {
+            v.setScaleX(1.04f);
+            v.setScaleY(1.04f);
+        }else {
+            v.setScaleX(1.0f);
+            v.setScaleY(1.0f);
         }
-
-        return false; // let the event propagate to other listeners
     }
 }
 
